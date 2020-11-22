e0593d406021bc4fe9b151f9aa596c91af3fb0f1
Jolyjona <jolyjonathan89@gmail.com>
2020-11-22 12:46:06 +0100

    feat(models): create API class to access swapi

---
+++ src/models/index.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import type { Film } from '../interfaces';
+
+
+export default class API {
+    #entrypoint: string = 'https://swapi.dev/dapi';
+
+    get<T>(url: string): Promise<T> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await axios.get(url);
+                resolve(response.data);
+            }
+            catch (error) {
+                reject('API_ERROR');
+            }
+        });
+    }
+
+    getfilm(filmNumber: number): Promise<Film> {
+        return this.get<Film>(`${this.#entrypoint}/films/${filmNumber}`);
+    };
+
+    getMultiple<T>(urls: string[]): Promise<T[]> {
+        return Promise.all(
+            urls.map((url) => this.get<T>(url))
+        );
+    }
+}
+
+
